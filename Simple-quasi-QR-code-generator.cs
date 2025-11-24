bb5ff824e0b826d2df7946b0ace54f432cc1ef6d
andkar94 <andkar94@gmail.com>
2025-11-24 18:18:24 +0100

    Dodanie kodu z simple-qr-generator

---
+++ Simple-quasi-QR-code-generator.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+
+Random random = new Random();
+int z = 20;
+
+while (true)
+{
+    Console.Clear();
+
+    string[,] matrix = new string[z,z];
+
+    for (int i = 0; i < matrix.GetLength(0)/1.5; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1)/1.6; j++)
+        {
+            int k = random.Next(0, z);
+            int l = random.Next(0, z);
+
+            matrix[k, l] = "o";
+                          
+        }
+    }
+ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    { 
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            string el = (matrix[i, j] != "o") ? " " : "o";
+            Console.Write(el + " ");           
+        }
+        Console.WriteLine();
+    }
+    Console.ReadKey();  
+}
+
