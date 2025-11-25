dc3a1fcaada33e3c35513bf94c0efde6b176f871
Karol Andrzejczak <andkar94@gmail.com>
2025-11-25 10:21:48 +0100

    Fix formatting

--- LINQ.cs
+++ LINQ.cs
@@ -1,10 +1,7 @@
-﻿
-int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+﻿int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
 var evenNumbers = numbers.Where(n => n % 2 == 0);
 var multipledby7 = numbers.Select(n => n * 7);
 
 Console.WriteLine(string.Join(", ", evenNumbers));
 Console.WriteLine(string.Join(", ", multipledby7));
-
-
